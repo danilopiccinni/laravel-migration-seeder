f4e43cd7abb0a13efdf8da35e50cfd5a19d2c667
danilopiccinni <danilopiccinni@gmail.com>
2023-05-12 15:50:54 +0200

    seeder tramite file csv

--- database/seeders/TrainsSeeder.php
+++ database/seeders/TrainsSeeder.php
@@ -18,21 +18,46 @@ class TrainsSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for($i=0 ; $i < 10 ; $i++) {
+        // for($i=0 ; $i < 10 ; $i++) {
+        //     $train = new Train();
+
+        //     $train->azienda = $faker->words(2, true);
+        //     $train->stazione_di_partenza = $faker->city();
+        //     $train->stazione_di_arrivo = $faker->city();
+        //     $train->orario_di_partenza = $faker->dateTime();
+        //     $train->orario_di_arrivo = $faker->dateTime();
+        //     $train->codice_treno = $faker->randomNumber(5 , true);
+        //     $train->numero_carrozze = $faker->randomNumber(2);
+        //     $train->in_orario = $faker->boolean();
+        //     $train->cancellato = $faker->boolean();
+
+        //     $train->save();
+        // }
+
+        $trainsFile = fopen(__DIR__ . '/../tabella_treni.csv', 'r');
+
+        $trainsCsv = fgetcsv($trainsFile);
+
+
+        $trainCsv = fgetcsv($trainsFile);
+
+        while ($trainCsv != false) {
+            
             $train = new Train();
 
-            $train->azienda = $faker->words(2, true);
-            $train->stazione_di_partenza = $faker->city();
-            $train->stazione_di_arrivo = $faker->city();
-            $train->orario_di_partenza = $faker->city();
-            $train->orario_di_partenza = $faker->dateTime();
-            $train->orario_di_arrivo = $faker->dateTime();
-            $train->codice_treno = $faker->randomNumber(5 , true);
-            $train->numero_carrozze = $faker->randomNumber(2);
-            $train->in_orario = $faker->boolean();
-            $train->cancellato = $faker->boolean();
+            $train->azienda = $trainCsv[0];
+            $train->stazione_di_partenza = $trainCsv[1];
+            $train->stazione_di_arrivo = $trainCsv[2];
+            $train->orario_di_partenza = $trainCsv[3];
+            $train->orario_di_arrivo = $trainCsv[4];
+            $train->codice_treno = $trainCsv[5];
+            $train->numero_carrozze = $trainCsv[6];
+            $train->in_orario = $trainCsv[7];
+            $train->cancellato = $trainCsv[8];
 
             $train->save();
+
+            $trainCsv = fgetcsv($trainsFile);
         }
     }
 }
